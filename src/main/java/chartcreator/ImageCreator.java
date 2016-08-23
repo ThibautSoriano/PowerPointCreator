52b8dd26fcaa15b19162ae2d86e6087446f4b6dc
Thibaut SORIANO <st405262@etu.unice.fr>
2016-08-23 17:08:26 +0200

    2e graphique : hommes / femmes

--- src/main/java/chartcreator/ImageCreator.java
+++ src/main/java/chartcreator/ImageCreator.java
@@ -2,35 +2,34 @@ package main.java.chartcreator;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Paint;
 import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.imageio.ImageIO;
 
-import org.apache.poi.sl.usermodel.PictureData;
-import org.apache.poi.util.IOUtils;
-import org.apache.poi.xslf.usermodel.XMLSlideShow;
-import org.apache.poi.xslf.usermodel.XSLFSlide;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.CategoryAxis;
 import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.labels.ItemLabelAnchor;
+import org.jfree.chart.labels.ItemLabelPosition;
 import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.chart.renderer.category.StandardBarPainter;
 import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.TextAnchor;
+
+import excelreader.Triplet;
 
 public class ImageCreator {
 
@@ -75,16 +74,74 @@ public class ImageCreator {
         return bufferedImage;
 	}
 	
+	public Image getDoubleValueChart(List<Triplet> triplets) {
+		
+		final BarRenderer renderer = new CustomRenderer(
+	            new Paint[] {new Color(132, 189, 0), new Color(255, 102, 0), Color.green,
+	                Color.yellow, Color.orange, Color.cyan,
+	                Color.magenta, Color.blue}
+	        );
+		
+		DefaultCategoryDataset barDataset = new DefaultCategoryDataset();
+		for (int i  = 0; i < triplets.size(); i++) {
+			barDataset.setValue(triplets.get(i).getValue(), "", triplets.get(i).getX());
+		}
+		
+	    JFreeChart chart = ChartFactory.createBarChart(
+	        "", "", "", barDataset,
+	        PlotOrientation.HORIZONTAL, false, true, false);
+	    
+	    CategoryPlot plot = (CategoryPlot) chart.getPlot();
+        plot.setRangeGridlinesVisible(false);
+        plot.setBackgroundPaint(Color.WHITE);
+        plot.setOutlineVisible(false);
+        
+        CategoryAxis domainAxis = plot.getDomainAxis();
+        domainAxis.setTickLabelFont(new Font("Calibri",Font.PLAIN,20));
+//	    domainAxis.setVisible(false);
+	    ValueAxis valueAxis = plot.getRangeAxis();
+	    valueAxis.setVisible(false);
+	    valueAxis.setUpperMargin(0.18);
+	    
+	    
+//	    BarRenderer renderer
+//        = (BarRenderer) plot.getRenderer();
+	    renderer.setMaximumBarWidth(.1);
+	    renderer.setShadowVisible(false);
+	    renderer.setBarPainter(new StandardBarPainter());
+        ItemLabelPosition p = new ItemLabelPosition(
+            ItemLabelAnchor.OUTSIDE3, TextAnchor.CENTER_LEFT, TextAnchor.CENTER, 0.0
+        );
+        renderer.setSeriesPositiveItemLabelPosition(0, p);
+		DecimalFormat pctFormat = new DecimalFormat("#.00%");
+		renderer.setSeriesItemLabelGenerator(0,
+                new StandardCategoryItemLabelGenerator("  {2}",NumberFormat.getNumberInstance()));
+		renderer.setBaseItemLabelFont(new Font("Calibri",Font.PLAIN,20));
+
+		renderer.setSeriesItemLabelsVisible(0, true);
+		plot.setRenderer(renderer);
+	    int width = 480;
+        int height = 240;
+        BufferedImage bufferedImage = chart.createBufferedImage(width, height);
+
+        return bufferedImage;
+	}
+	
 	public static void main(String[] args) throws IOException {
 		ImageCreator MERGUEZ = new ImageCreator();
 		
-		File outputfile = new File("merguez.png");
-		List<Float> top = new ArrayList<>();
-		top.add(0.2233f);
-		top.add(0.18f);
-		top.add(0.15f);
-		ImageIO.write((RenderedImage) MERGUEZ.getTop3BarChart(top), "png", outputfile);
+//		File outputfile = new File("merguez.png");
+//		List<Float> top = new ArrayList<>();
+//		top.add(0.2233f);
+//		top.add(0.18f);
+//		top.add(0.15f);
+//		ImageIO.write((RenderedImage) MERGUEZ.getTop3BarChart(top), "png", outputfile);
 		
+		File output = new File("zhengqin.png");
+		List<Triplet> trip = new ArrayList<>();
+		trip.add(new Triplet("Férfi", "zhengqin", 76));
+		trip.add(new Triplet("Nő", "zhengqin", 56));
+		ImageIO.write((RenderedImage) MERGUEZ.getDoubleValueChart(trip), "png", output);
 		
 //		FileOutputStream out;
 //        try {
